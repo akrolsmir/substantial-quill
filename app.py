ea9510e723ce30c21dd179be9853ec02b5d72eb8
Austin Chen <akrolsmir@gmail.com>
2020-08-13 16:13:36 -0700

    Initial setup for simple Heroku app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.beta_set_page_config(
+    page_title="{0}", page_icon="{1}", layout="wide", initial_sidebar_state="collapsed"
+)
+
+options = pd.DataFrame({"Options": ["radio", "multiselect", "selectbox"]})
+
+st.radio("Dataframe as input for radio buttons", options)
